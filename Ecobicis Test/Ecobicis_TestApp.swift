450a5f847d321c1996b4c9db7f6d55d12527a23a
Meldrick Trujeque <meldrick.dev22@gmail.com>
2023-12-16 12:00:10 -0600

    Initial Commit

---
+++ Ecobicis Test/Ecobicis_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecobicis_TestApp.swift
+//  Ecobicis Test
+//
+//  Created by Meldrick Trujeque on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Ecobicis_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
